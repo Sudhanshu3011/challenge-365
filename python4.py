04c058c2000f533eeba79845e8a71730b812b07c
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-10 16:47:41 +0530

    Calculator

---
+++ python4.py
@@ -0,0 +1,9 @@
+a=45
+b=18
+print("The addition of", a, "+", b,"is: ",a+b)
+print("The substraction of", a, "-", b,"is: ",a-b)
+print("The multiply of", a, "*", b,"is: ",a*b)
+print("The division of", a, "/", b,"is: ",a/b)
+print("The divisor of", a, "//", b,"is: ",a//b)
+print("The modulo of", a, "%", b,"is: ",a%b)
+print("The exponential of", a, "**", b,"is: ",a**b)
