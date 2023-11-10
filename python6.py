dbeea4088e262dd7d12187c1e8af02336a9b4170
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-10 17:59:06 +0530

    Input_taking

---
+++ python6.py
@@ -0,0 +1,19 @@
+a=input()
+print("Name",a)
+
+b=input("Enter your name:")
+print("Your name is:",b)
+
+x=input("Enter your first name is:")
+y=input("Enter your last name is:")
+print(x + y)
+
+num1=input("Enter the number[1]:")
+num2=input("Enter the number[2]:")
+print("The sum of string is",num1+num2)
+numb1=int(num1)
+numb2=int(num2)
+print("The sum of the",numb1,"and",numb2,"is :",numb1+numb2)
+
+
+
