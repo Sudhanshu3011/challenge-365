5e3e10f98971eb9799a144f3d140e9fa490561de
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-10 17:10:04 +0530

    Typecastimg

---
+++ python5.py
@@ -0,0 +1,14 @@
+#explicit typecastion
+b="18"
+c="45"
+'''changing of the data type of b and c'''
+b_no=int(b)
+c_no=int(c)
+sumation=b_no+c_no
+print("The sum of b and c is:",sumation)
+#implicit typecasting
+d=27.8
+e=35
+#the data type is implicitely changes to the float of sum 
+sum=d+e
+print("The sum of d and c is :",sum)
