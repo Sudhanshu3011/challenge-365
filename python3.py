f36a8ff042e397afcaf105d03aced5e446a8859a
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-10 18:23:15 +0530

    Strings

---
+++ python3.py
@@ -0,0 +1,9 @@
+# This is how the comment line is generated.
+#Using different escape sequences
+'''
+This is the multi line statements.
+This is the multi line statements.
+This is the multi line statements.
+'''
+print("Myself \"sudhanshu shekhar\"\nA hustling coder" )
+print("Age",19,sep=" ",end="2004")
---
+++ python7.py
@@ -0,0 +1,14 @@
+sudhanshu="""Myself Sudhanshu shekhar\nKeep hustling \"Till success\""""
+name="siddhu"
+print("Hello,"+name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+#printing using the loop
+print(sudhanshu)
+print("Using the loop character printing")
+for character in sudhanshu:
+    print(character)
