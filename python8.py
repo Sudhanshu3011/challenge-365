4e7031cf0e0325dbd881ac1fb8333a1ca85e0b33
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-11 11:14:54 +0530

    string_slicing

---
+++ python8.py
@@ -0,0 +1,20 @@
+names="my fav robot"
+print(names[8:15])
+print(names)
+print(len(names))
+
+ #the slicing of the string also work for the negative values
+name ="Sudhanshu"
+print(len(name))
+print(name[-4:-2])
+print(name[len(name)-4:len(name)-2])
+#Takes the whole string as the print 0:9
+print(name[:])
+#Takes the whole string as the print 0:7
+print(name[:7])
+#Takes the whole string as the print 0:9
+print(name[0:])
+
+
+
+
