6de74e16f6d97a316ed66898f4cc8851b83fd1fb
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-11 18:25:58 +0530

    string-operation

---
+++ python9.py
@@ -0,0 +1,16 @@
+#strings are immutable
+
+word="MYself_Sudhanshu-shekhar"
+word2="intro is Here"
+word1="!!Sudhanshu-shekhar  Sudhanshu!!"
+print(word.upper())
+print(word.lower())
+print(word1.rstrip("!"))
+print(word1.replace("Sudhanshu","siddhu"))
+print(word2.capitalize())
+print(word.center(50))
+print(word1.count("Sudhanshu"))
+print(word1.endswith("!!"))
+print(word2.find("is"))
+print(word2.index("is"))
+
