db50d68e1490008c335e77e500d8295d4b9d97ff
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-13 11:56:56 +0530

    While&elseup

---
+++ python15.py
@@ -0,0 +1,13 @@
+count=5
+while(count>0):
+    print(count)
+    count=count-1
+
+#using else with the while loop
+num=0
+while(num<5):
+    print(num)
+    num=num+1
+else:
+    print("The else statement is executed as the while loop ends.")
+
