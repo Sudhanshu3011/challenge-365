7b03dd77960b9b1f1ff4c564032b0c6b89382034
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-13 18:35:33 +0530

    list_methods

--- python16.py
+++ python16.py
@@ -12,7 +12,7 @@ def comaprision(a,b):
 def table(a):
     for k in range(1,11):
         print(a,"*",k,"=",a*k)
-#to pass out the statement and use in furhter calls  
+# to pass out the statement and use in furhter calls  
 def lcm(a):
     pass
 
@@ -21,3 +21,24 @@ b=int(input("Enter the B:"))
 calculatesum(a,b)
 comaprision(a,b)
 table(a)
+
+#different types of function calls 
+def reamainder(a=10, b=2):
+    c=a%b
+    print(c)
+reamainder(b=5)
+reamainder(a=12)
+reamainder(b=6,a=36)
+
+def average(*num):
+    sum=0
+    for i in num:
+        sum=sum+i
+    # print("Average of the number is :",sum/len(num))
+    # return sum/len(num)
+
+average(5,6,9,10,53)
+
+#return functions
+c=average(1,2,3,4,5)
+print(c)
---
+++ python18.py
@@ -0,0 +1,40 @@
+lst=[i for i in range(11)]
+print(lst)
+#reverse the list
+lst.reverse()
+print(lst)
+
+lst1=[12,56,83,67,9,0,3,45,72]
+print(lst1)
+#ascending order
+lst1.sort()
+print(lst1)
+#descending order
+lst.sort(reverse=True)
+print(lst)
+
+#reverse the list
+lst.reverse()
+print(lst)
+
+#index of the element in the list
+print(lst1.append(67))
+print(lst1.index[67])
+
+#count method 
+print(lst.count[67])
+
+#copy a function
+lst2=lst1.copy()
+print(lst)
+
+#inserting at any position
+lst.insert(3,45)
+
+#extend function
+s=[18,45,25,17]
+v=[33,19,7,67]
+#here the v get changed with the newly added values if we dont need to 
+#change it then k=v+s
+v.extend(s)
+print(v)
