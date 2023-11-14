81a0f521e8af7155d99d2d7659fa90f17c9ef336
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-14 08:52:19 +0530

    match case
    match case

---
+++ python13.py
@@ -0,0 +1,25 @@
+#switch case
+print("Enter a no. less than 10.")
+a=int(input("Enter the number:"))
+
+match a:
+    case 1:
+        print("You selected 1.")
+    case 2:
+        print("You selected 2.")
+    case 3:
+        print("You selected 3.")
+    case 4:
+        print("You selected 4.")
+    case 5:
+        print("You selected 5.")
+    case 6:
+        print("You selected 6.")
+    case 7:
+        print("You selected 7.")
+    case 8:
+        print("You selected 8.")
+    case 9:
+        print("You selected 9.")
+    case _:
+        print("You selected 0.")
