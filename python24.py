9eae94cafd62b3fe0ef17326932e8d13133f05ba
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-15 08:47:01 +0530

    Dictionary

--- python24.py
+++ python24.py
@@ -16,3 +16,12 @@ for i in dict.keys():
 
 for key,value in dict.items():
     print(f"The values {value} of the corresponding key {key}.")
+
+ep1={1:67,2:89,3:32,4:90}
+ep2={5:89,6:98}
+
+ep1.update(ep2)
+print(ep1)
+ep1.pop(1)
+
+del ep1[122]
