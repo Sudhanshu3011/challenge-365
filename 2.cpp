96b912b8457476b63f63a9057f85963325c0a0b2
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-16 16:40:17 +0530

    Second C++

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+// variables and the declaration
+int main()
+{
+    int x;
+    x=6;
+    cout<<x;
+    cout<<"My self";
+    cout<<"\n";
+    cout<<"endline";
+    cout<<"Introduction";
+} // namespace name
