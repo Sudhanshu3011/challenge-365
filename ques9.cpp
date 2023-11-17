fe16e1971f8a7d5a0cb1d0a427f3a36175c69b25
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-17 14:59:25 +0530

    Loop questions

---
+++ ques9.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch='A';
+    for(int i=0;i<26;i++)
+    {
+        cout<<"The Ascii value is "<<int(ch)<<"of character"<<ch<<endl;
+        ch++;
+    }
+}
