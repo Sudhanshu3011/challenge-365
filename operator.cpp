5d48c5dab0b02ec8b0dfe7d9a444c2ef624a2cc0
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-17 09:00:34 +0530

    Operators

---
+++ operator.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+//using all operators
+int main()
+{
+    int x,y;
+     cout<<"Enter the first number:";
+    cin>>x;
+    cout<<"Enter the second number:";
+    cin>>y;
+    cout<<"The different operation are:"<<endl;
+    cout<<"sum: "<<x+y<<endl;
+    cout<<"substract: "<<x-y<<endl;
+    cout<<"multiply: "<<x*y<<endl;
+    cout<<"division: "<<x/y<<endl;
+    cout<<"modulo: "<<x%y<<endl;
+}
