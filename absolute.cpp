7fcc9c66bac9dde73ea5f98140af72e22f7aaacf
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-17 10:08:54 +0530

    absolute value

---
+++ absolute.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+
+    cout << "Enter a number:";
+    cin >> n;
+    cout<<"The absolute value of the number.";
+    cout<<abs(n);
+}
