f2591e60ca57682acfb3d9e4fe80f8cdade1d9f8
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-17 08:00:03 +0530

    Simple interest

---
+++ 4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float p=2000;//principal
+    float r=20;//rate 
+    float t=3;//time in years 
+    float si=(p*r*t)/100;
+    cout<<"The simple intrest of the given amount:"<<endl;
+    cout<<si;
+
+}
