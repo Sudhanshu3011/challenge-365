b018204978b6bb26344fb67d8ee49583ad0e439d
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-17 08:01:13 +0530

    Square

---
+++ 5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+//square of the number
+int main()
+{
+    int x;
+    cout<<"Enter the number:";
+    cin>>x;
+    //cout<<endl;
+    cout<<"The square is :";
+    cout<<x*x;
+
+}
