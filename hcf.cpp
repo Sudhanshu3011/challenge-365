f80a51e760b012cb4e0df280e842a2e241b67f78
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-17 11:37:15 +0530

    HCF

---
+++ hcf.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, hcf;
+    cout << "Enter the Number:";
+    cin >> n;
+
+    // highest common factor of the number
+    for (int i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            hcf = i;
+        }
+    }
+
+    cout << "The HCF of the number " << n << "is :" << hcf;
+}
