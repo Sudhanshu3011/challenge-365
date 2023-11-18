ac5a055aa06055501bf8f112b66ce58aa9b31f79
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-18 18:02:28 +0530

    built in function

---
+++ libraryfun.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    cout<<cbrt(10)<<endl;
+    cout<<min(12,11)<<endl;
+    cout<<pow(2,3)<<endl;
+
+    //and many more built function are avialable
+}
