7f077814de35e99cee4bbd1318bac1d41e380c93
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-22 18:40:49 +0530

    os functions

---
+++ python30.py
@@ -0,0 +1,22 @@
+# import os
+
+#make a 100 files in the directary 
+#if os.path.exists("data"):
+#   os.mkdir("data")
+
+#for i in range(0,100):
+#   os.mkdir(f"data/Day{i+1}")
+#   
+
+#to rename the files 
+#for i in range(0,100):
+#   os.rename(f"data/Day{i+1}",f"data/Tutorial{i+1}")
+#   
+
+#folders =os.listdir("data")
+#print(folders)
+
+#for folder in floders:
+#   print(folder)
+#   print()
+
