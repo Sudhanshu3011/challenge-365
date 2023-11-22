6a562bcfdb7948a898e8521a7cf1bef52702cbe2
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-11-22 17:56:11 +0530

    library function

---
+++ python29.py
@@ -0,0 +1,19 @@
+import math
+
+result=math.sqrt(49)
+print(result)
+
+import math as m
+res=m.sqrt(49)
+print(res)
+
+from math import pi,sqrt as s
+re=s(121)*pi
+print(re)
+
+import math as m
+result1=m.sqrt(21)*m.pi
+print(result1)
+
+print(dir(math))
+
