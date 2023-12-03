228be037c51a195aa0adabfd335d1d92f66354d7
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2023-12-03 16:48:25 +0530

    OOPS-varios magic method

---
+++ python46.py
@@ -0,0 +1,8 @@
+from python45 import Bus
+
+#this will redirect to the python 45 and fetches up the whole class from there 
+
+B1=Bus("GJ23VD2375","Sleeper_AC","Rajkot-Kutch")
+print(str(B1))
+print(repr(B1))
+B1()
