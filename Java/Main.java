9abae0b3efed524fc67388de57e644da99a450d8
Sudhanshu3011 <sudhanshushekarkumar@gmail.com>
2024-03-15 19:40:05 +0530

    Abstract class - Java

---
+++ Java/Main.java
@@ -0,0 +1,58 @@
+abstract class Shape {
+    abstract double area();
+}
+
+class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    double area() {
+        return 0.5 * base * height;
+    }
+}
+
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double area() {
+        return length * width;
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(5, 4);
+        Rectangle rectangle = new Rectangle(6, 3);
+        Circle circle = new Circle(4);
+
+        System.out.println("Area of Triangle: " + triangle.area());
+        System.out.println("Area of Rectangle: " + rectangle.area());
+        System.out.println("Area of Circle: " + circle.area());
+    }
+}
